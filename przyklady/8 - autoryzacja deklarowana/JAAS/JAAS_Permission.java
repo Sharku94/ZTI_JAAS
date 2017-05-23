d4b701c67740784a8eab7f063d205b46cf2d4677
Sharku94 <olaf.schab@fis.agh.edu.pl>
2017-05-23 20:41:22 +0200

    Przyklad 8
    Naprawione

--- przyklady/8 - autoryzacja deklarowana/JAAS/JAAS_Permission.java
+++ przyklady/8 - autoryzacja deklarowana/JAAS/JAAS_Permission.java
@@ -2,35 +2,11 @@ package JAAS;
 
 import java.security.Permission;
 
-public class JAAS_Permission extends Permission {
+public class JAAS_Permission extends BasicPermission {
 
 	public JAAS_Permission(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public String getActions() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int hashCode() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean implies(Permission permission) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
